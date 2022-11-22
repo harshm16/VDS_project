014c0f74a9b27ac278a5eb050f3806ae56cd127b
shaikjunaidmohd <65023602+shaikjunaidmohd@users.noreply.github.com>
2022-11-22 01:28:48 -0600

    Radial stack & Arc

--- src/js/script.js
+++ src/js/script.js
@@ -5,15 +5,18 @@ let showFlag = 0;
 let temperatureFactor = new Array();
 let count = 0;
 
+
 function tableFunction(){
     var table = document.createElement('table');
     table.setAttribute('border-collapse', 'collapse');
     table.setAttribute('background-color','#96D4D4');
+    table.setAttribute('id','myTable');
     var row = table.insertRow(0);
     lSize = l.length;
     for(let i = 0 ; i< l.length;i++){
         var cell = row.insertCell(i-1);
         cell.setAttribute('align','center');
+        cell.setAttribute('size','10px');
         cell.appendChild(document.createTextNode(l[i-1]));
     }
     // console.log(l)
@@ -24,6 +27,7 @@ function tableFunction(){
             len = Number(ruler.attr("data-items")) || -1,
             item = $(document.createElement("li")),
             i;
+            //this.change(5);
             // console.log(lSize);
         for (i = 0; i < l.length; i++) {
             if((i+1) % 5 == 0){
@@ -50,12 +54,11 @@ function pdbInput(){
     inp = document.getElementById("myText").value;
 }
 
-
 function ndGraph(data){
-
-    var margin = { top: 30, right: 30, bottom: 40, left: 250 },
-    width = 560 - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    
+    var margin = { top: 0, right: 30, bottom: 40, left: 250 },
+    width = 460 - margin.right,
+    height = 270 - margin.top - margin.bottom;
 
 
     min = d3.min(data);
@@ -80,7 +83,7 @@ function ndGraph(data){
 
 
     var svg = d3
-    .select("body")
+    .select("#SE")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -184,6 +187,7 @@ function ndGraph(data){
     
 
 
+
 function myFunction(x, y) {
 
     if (x === undefined) {
@@ -207,6 +211,9 @@ function myFunction(x, y) {
     var count = 0;
     let config = { backgroundColor: 'black' };
     let viewer = $3Dmol.createViewer( element, config );
+
+    
+
     let colorAsSnake = function(atom) {
         if(flag){
             flag = false;
@@ -232,18 +239,75 @@ function myFunction(x, y) {
     viewer.setStyle({chain: 'A'}, {cartoon: {colorfunc: colorAsSnake}});
     viewer.render();
 
+
+    ////////////////////////////////////////////////////
+    
     
     if(showFlag == 0){
-
+        // ndGraph();
         ndGraph(temperatureFactor);
         tableFunction();
-        
         barChart(0)
- 
+        // document.addEventListener('click', function (event) {
+        //     if (!event.target.matches('.bar')) return;
+            
+        //     console.log(event.target);
+        // }, false);
+
+
+
+
         showFlag = 1;
     }
     
+
+    
     });
+    // let element1 = $('#container-02');
+    // let init1 = 0;
+    // let flag1 = true;
+    // var count1 = 0;
+    // let config1 = { backgroundColor: 'black' };
+    // let viewer1 = $3Dmol.createViewer( element1, config1 );
+    // let partitions = Math.round(211/9);
+    // let color_count = 0;
+    // let colors_scale = ["#f7fcf0","#e0f3db","#ccebc5","#a8ddb5","#7bccc4","#4eb3d3","#2b8cbe","#0868ac","#084081"];
+    // let colorAsSnake1 = function(atom) {
+    //     if(flag){
+    //         flag = false;
+    //         init = atom.resi;
+    //     }
+
+    //     if(count1 == partitions){
+    //         count1 = 0;
+    //         color_count++;
+    //         return colors_scale[color_count];
+    //     }
+    //     count1++;
+    //     return colors_scale[color_count];
+    //     // l[count] = " " + atom.resn + " ";
+    //     // temperatureFactor[count] = atom.b;
+    //     // count += 1;
+    //     // console.log(atom.resi);
+    //     // return li.includes(atom.resi) ? 'white' : 'green';
+    // };
+    // $3Dmol.download("PDB-samples/sample.pdb",viewer1,{},function(){
+    //     // viewer.setStyle({chain: 'A', within:{distance: 10, sel:{chain: 'B'}}}, {sphere:{}});
+    //     viewer1.setHoverable({},true,function(atom,viewer1,event,container) {
+    //     if(!atom.label) {
+    //         atom.label = viewer1.addLabel(atom.resn+":"+atom.atom,{position: atom, backgroundColor: 'mintcream', fontColor:'black'});
+    //     }},
+    //     function(atom) {
+    //         if(atom.label) {
+    //             viewer1.removeLabel(atom.label);
+    //             delete atom.label;
+    //     }});
+    //     viewer1.setStyle({chain: 'A'}, {cartoon: {colorfunc: colorAsSnake1}});
+    //     viewer1.render();
+
+        
+    // });
+
 };
 
 function barChart(instance){
@@ -265,8 +329,8 @@ function barChart(instance){
         tooltip.html('<i>Index:</i> <b><span style="color:#DEDC00"> ' + i + '</span></b> <br>\
         <i>Amino Acid:</i> <b><span style="color:#DEDC00"> ' + l[i] + '</span></b>  <br>\
         <i>Temperature factor:<i> <b><span style="color:#DEDC00">' + temperatureFactor[i] + '</span></b>')
-        .style('left', (d3.event.pageX + 700) + 'px')
-        .style('top', (d3.event.pageY - 20) + 'px')
+        .style('left', (d3.event.pageX - 50) + 'px')
+        .style('top', (d3.event.pageY - 50) + 'px')
     }
 
     //tooltip
@@ -317,7 +381,7 @@ function barChart(instance){
     if (instance === 0){
         var margin = {top: 50, right: 50, bottom: 50, left: 5}
         , width = 10000 - margin.left - margin.right  
-        , height = 175 - margin.top - margin.bottom; 
+        , height = 70 - margin.top - margin.bottom; 
 
         var n = l.length;
 
@@ -411,7 +475,6 @@ function barChart(instance){
 };
 
 
-
 function myreset() {
 
     var li = []
@@ -477,7 +540,6 @@ function myreset() {
 
     // count = 0;
     barChart(1)
-
     // d3.selectAll("bar")
     // .attr("fill", "#FFFFFF")
     // .attr("fill-opacity", 0)
@@ -485,3 +547,4 @@ function myreset() {
     // .attr('stroke-width', 3);
 
 };
+
